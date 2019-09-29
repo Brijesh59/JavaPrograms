e2f40cd1fbc67572377ae648f671239738039a14
Brijesh Kumar <kumarbrijesh9021@gmail.com>
2019-09-29 12:48:34 +0530

    Update Program2.java

--- String/Program2.java
+++ String/Program2.java
@@ -1,3 +1,30 @@
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+1. Open brackets must be closed by the same type of brackets.
+2. Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Input: "()"
+Output: true
+
+Input: "()[]{}"
+Output: true
+
+Input: "(]"
+Output: false
+
+Input: "([)]"
+Output: false
+
+Input: "{[]}"
+Output: true
+
+NOTE: This is Leetcode Problem 20. [ https://leetcode.com/problems/valid-parentheses/ ]
+*/
+
+
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
